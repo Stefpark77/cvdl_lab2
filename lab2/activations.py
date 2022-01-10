871c7f4ed0c51175998025d8f41864c4167363eb
Stefpark77 <stefanturcut@yahoo.com>
2022-01-10 12:08:32 +0200

    Initial commit

---
+++ lab2/activations.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def softmax(x, t=1):
+    """"
+    Applies the softmax temperature on the input x, using the temperature t
+    """
+    X = x - max(x)
+    return np.exp(X/t) / np.sum(np.exp(X/t))
